98c44c3ae242be496246d56b16f0dd1d57fe835c
vietvulong <vietvulong@protonmail.com>
2022-06-14 20:24:12 +0700

    This is for starting point to build React plus TainwindCSS.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
+import desert from "../../images/desert.jpg"
+import logo from "../../images/logo.jpg"
+
+
+
 const Home = (props) => {
 
     return (
         <div className="w-full">
-            <div className="w-1/4 h-4 bg-customPrimary"></div>
-            <div className="w-3/4"></div>
+        <img className="absolute top-0 left-0" src={desert} style={{maxWidth:"1440"}} />
+        <div className="" style={{maxWidth:"269"}}>
+            <img src={logo} style={{maxWidth:"47"}} />
+            <p>Nattose</p>        
+        </div>
+
+            <div className="flex justify-center pt-6" >
+                <div className="rounded-lg border border-gray-700 shadow-xl" style={{maxWidth:"638"}}>
+                    <div>sdfsdfsfsfsfd</div>
+                </div>
+            </div>
         </div>
     )
 }
